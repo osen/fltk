4cf9dc1329353d47b432e0096346f55836fac421
Karsten Pedersen <kpedersen@>
2021-11-01 19:40:33 +0000

    Added .h/.cxx boilerplate.

--- src/Fl_Flow.cxx
+++ src/Fl_Flow.cxx
@@ -1,6 +1,23 @@
+//
+// Flow widget for the Fast Light Tool Kit (FLTK).
+//
+// Copyright 2021 by Karsten Pedersen and others.
+//
+// This library is free software. Distribution and use rights are outlined in
+// the file "COPYING" which should have been included with this file.  If this
+// file is missing or damaged, see the license at:
+//
+//     https://www.fltk.org/COPYING.php
+//
+// Please see the following page on how to report bugs and issues:
+//
+//     https://www.fltk.org/bugs.php
+//
+
 #include <FL/Fl_Flow.H>
 
 #include <stdio.h>
+#include <string.h>
 
 static void set_position(Fl_Widget *widget, int _x, int _y) {
   Fl_Group *parent = widget->parent();
